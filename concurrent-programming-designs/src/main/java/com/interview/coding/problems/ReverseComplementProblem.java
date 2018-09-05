759e822691018085ea83b97f63283a96416a5388
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-05 22:03:05 +0530

    commit related to code Standardization according to google code style.

--- concurrent-programming-designs/src/main/java/com/interview/coding/problems/ReverseComplementProblem.java
+++ concurrent-programming-designs/src/main/java/com/interview/coding/problems/ReverseComplementProblem.java
@@ -11,15 +11,17 @@ package com.interview.coding.problems;
  */
 
 /**
- *  REVERSE COMPLEMENT PROBLEM 
- *
- *  Find the reverse complement of a DNA string. 
- *
- *  Given:  A DNA string Pattern. 
- *  Return: Pattern*, the reverse complement of Pattern. 
+ * REVERSE COMPLEMENT PROBLEM
+ * <p>
+ * Find the reverse complement of a DNA string.
+ * <p>
+ * Given:  A DNA string Pattern.
+ * Return: Pattern*, the reverse complement of Pattern.
  */
-
 public class ReverseComplementProblem {
+  /**
+   * @param args none
+   */
   public static void main(String[] args) {
     // DEFINE "dna"!!!
     String dna = "";
@@ -28,22 +30,26 @@ public class ReverseComplementProblem {
     System.out.println(reverseComplement(dna));
   }
 
+  /**
+   * @param dna dna
+   * @return reverse complement of passed dna
+   */
   public static String reverseComplement(String dna) {
     String out = "";
     for (int i = dna.length() - 1; i >= 0; --i) {
       char curr = dna.charAt(i);
-        if (curr == 'A') {
-            out += 'T';
-        } else if (curr == 'T') {
-            out += 'A';
-        } else if (curr == 'C') {
-            out += 'G';
-        } else if (curr == 'G') {
-            out += 'C';
-        } else {
-            System.out.println("ERROR: Input is not a DNA Sequence.");
-            System.exit(-1);
-        }
+      if (curr == 'A') {
+        out += 'T';
+      } else if (curr == 'T') {
+        out += 'A';
+      } else if (curr == 'C') {
+        out += 'G';
+      } else if (curr == 'G') {
+        out += 'C';
+      } else {
+        System.out.println("ERROR: Input is not a DNA Sequence.");
+        System.exit(-1);
+      }
     }
     return out;
   }
--- concurrent-programming-designs/src/main/java/com/interview/coding/problems/Visa.java
+++ concurrent-programming-designs/src/main/java/com/interview/coding/problems/Visa.java
@@ -1,9 +1,17 @@
 package com.interview.coding.problems;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Visa
+ */
 public class Visa {
 
+  /**
+   * @param args none
+   */
   public static void main(String[] args) {
     List<String> l = new LinkedList<>();
     l.add("4 2");
@@ -14,6 +22,10 @@ public class Visa {
     System.out.println(s);
   }
 
+  /**
+   * @param abvalues abvalues
+   * @return sum
+   */
   static String sum(List<String> abvalues) {
 
     List<Integer> num = new LinkedList<>();
@@ -26,9 +38,9 @@ public class Visa {
       num.add(Integer.valueOf(num_den[0]));
       den.add(Integer.valueOf(num_den[1]));
     }
-	  if (num.size() != den.size()) {
-		  return "";
-	  }
+    if (num.size() != den.size()) {
+      return "";
+    }
     int gcd = 0;
     // for i in xrange(0, len(den)-1):
 
@@ -39,19 +51,19 @@ public class Visa {
 
   static int gcd(int a, int b) {
     // Everything divides 0
-	  if (a == 0 || b == 0) {
-		  return 0;
-	  }
+    if (a == 0 || b == 0) {
+      return 0;
+    }
 
     // base case
-	  if (a == b) {
-		  return a;
-	  }
+    if (a == b) {
+      return a;
+    }
 
     // a is greater
-	  if (a > b) {
-		  return gcd(a - b, b);
-	  }
+    if (a > b) {
+      return gcd(a - b, b);
+    }
     return gcd(a, b - a);
   }
 
@@ -72,65 +84,5 @@ public class Visa {
       }
       ++lcm;
     }
-
   }
-
-//	   def lcmm(self, *args):
-//	      """Return lcm of args."""   
-//	      return reduce(self.lcm, args)    
-
 }
-//
-// #parameter num, a list of numerator
-// #parameter den, a list of denominator
-// def ration_sum(self, num, den):
-//
-// if len(num) != len(den):
-//
-// print "The length of numerators is not equal to denominators"
-// return
-//
-// #find the gratest common factor for denominators
-// gcd = 0
-// for i in xrange(0, len(den)-1):
-//
-// gcd = self.gcd(den[i], den[i+1])
-//
-// #find the least common multiple for denominators
-// lcm = self.lcmm(*den)
-//
-// num_sum = 0 #initialize the sum of num
-//
-// for i in xrange(0, len(num)):
-// num_sum += num[i] * (lcm/den[i])
-//
-// gcd_sum = self.gcd(num_sum, lcm) #greatest common factor of sum num and den
-//
-// return num_sum/gcd_sum, lcm/gcd_sum
-
-
-//// Counts the no of manipulations required
-//public static int countManipulations(String s1, String s2)
-//{
-//		if ((s1.length() % 2) != 0)
-//        return -1;
-//		
-//    int count = 0;
-//
-//    // store the count of character
-//    int char_count[] = new int[26];
-//
-//    // iterate though the first String and update 
-//    // count
-//    for (int i = 0; i < s1.length(); i++) 
-//        char_count[s1.charAt(i) - 'a']++;        
-//
-//    // iterate through the second string
-//    // update char_count.
-//    // if character is not found in char_count
-//    // then increase count
-//    for (int i = 0; i < s2.length(); i++) 
-//        if (char_count[s2.charAt(i) - 'a']-- <= 0)
-//            count++;
-//    return count;
-//}
