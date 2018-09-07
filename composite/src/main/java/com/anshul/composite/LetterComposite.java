16eb0498b2e53c78f8cd526a9db8e760f1f280ab
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-07 22:16:47 +0530

    compostie design pattern

--- composite/src/main/java/com/anshul/composite/LetterComposite.java
+++ composite/src/main/java/com/anshul/composite/LetterComposite.java
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.iluwatar.composite;
+package com.anshul.composite;
 
 import java.util.ArrayList;
 import java.util.List;
