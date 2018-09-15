01dbf77f6420efbebe390d8dfa4ee482e9d2bfa6
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-15 09:03:33 +0530

    Adding Test classes for Drink/Health clients

---
+++ springmvc-raml-based-api-design/drinks-client/src/test/java/com/phoenixnap/oss/sample/client/test/TestHealthCheckClient.java
@@ -0,0 +1,57 @@
+/**
+ * The MIT License
+ * Copyright (c) 2018 Anshul Mohil
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.phoenixnap.oss.sample.client.test;
+
+import com.phoenixnap.oss.sample.client.HealthCheckClient;
+import com.phoenixnap.oss.sample.client.model.GetHealthCheckResponse;
+import com.phoenixnap.oss.sample.main.ClientLauncher;
+import com.phoenixnap.oss.sample.server.ServerLauncher;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ServerLauncher.class, ClientLauncher.class}, webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+public class TestHealthCheckClient {
+
+  @Autowired
+  private HealthCheckClient healthCheckClient;
+
+  @Test
+  public void getHealthCheckIntegrationTest() throws Exception {
+    ResponseEntity<GetHealthCheckResponse> healthCheckResponse = healthCheckClient.getHealthCheck();
+
+    Assert.assertEquals(HttpStatus.OK, healthCheckResponse.getStatusCode());
+    Assert.assertNotNull(healthCheckResponse.getBody());
+  }
+}
