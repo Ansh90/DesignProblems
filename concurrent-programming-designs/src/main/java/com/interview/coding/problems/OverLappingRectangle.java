badae785d58f2065a90a503fd5fafb6a0fa6ed7a
Anshul Mohil <anshulmohil@hotmail.com>
2018-09-19 09:47:54 +0530

    Rakuten online code problem "Overlapping rectangular area"

--- concurrent-programming-designs/src/main/java/com/interview/coding/problems/OverLappingRectangle.java
+++ concurrent-programming-designs/src/main/java/com/interview/coding/problems/OverLappingRectangle.java
@@ -1,5 +1,24 @@
 package com.interview.coding.problems;
 
+//Find the total area covered by two rectilinear rectangles in a 2D plane.
+//
+//Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+//
+//    https://leetcode.com/static/images/problemset/rectangle_area.png
+//
+//
+//
+//Rectangle Area
+//
+//Example:
+//
+//Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+//Output: 45
+//Note:
+//
+//Assume that the total area is never beyond the maximum possible value of int.
+
+
 /**
  * OverLappingRectangle
  */
